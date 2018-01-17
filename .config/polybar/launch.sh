12435b73e4c0e4b021870c2c8ddabe6b11d105f6
Miguel Duarte <gamer-go@hotmail.com>
2018-01-17 00:56:43 +0000

    Added polybar stuff and changed the startup options in i3 conf file to stop using i3blocks. Preserving the old i3blocks setup as failsafe

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch example bar for the time being
+polybar example &
+
+
+echo "Bars launched..."
+
+#For future testing
+#source "${HOME}/.cache/wal/colors.sh"
+#background=$color0
+#background_alt=$color3
+#foreground=$color15
+#foreground_alt=$color2
+#highlight=$color4
