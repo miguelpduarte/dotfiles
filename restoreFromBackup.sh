6f9c19f26c4f9f077f862fa61b9c7740e554f952
Miguel Duarte <gamer-go@hotmail.com>
2018-01-19 20:13:01 +0000

    Fixed some problems in the restore from backup script, and now successfully used both scripts :D

--- restoreFromBackup.sh
+++ restoreFromBackup.sh
@@ -3,6 +3,11 @@
 # Should match the generateBackup.sh script used
 # Run with sudo to give permission for operations done here
 
+#Common fixes:
+#Some are already contemplated but for example not having read access to sources.list* caused problems
+#Other than that, "dpkg: warning: package not in database" ocurrs if the package in "my-packages.list" has packages that are not in the added repos
+#That has to be fixed by manually editing that file and removing the packages that cause problems (probably are packages that you need to download the .deb file for or compile from source)
+
 #Restoring the system connections info
 sudo cp -R $HOME/backup-folder/system-connections /etc/NetworkManager/
 
@@ -18,6 +23,8 @@ sudo cp $HOME/backup-folder/hosts /etc/hosts
 #Reinstalling the saved packages
 sudo apt-key add $HOME/backup-folder/Repo.keys
 sudo cp -R $HOME/backup-folder/sources.list* /etc/apt/
+#Adding read permissions, prevents "dpkg: warning: package not in database" errors that cause the installations to fail
+sudo chmod +r /etc/apt/sources.list* -R
 sudo apt-get update
 sudo apt-get install dselect
 sudo dselect update
