85de8fcefbfe4aa54e6bcb7afe8fc3e2f207ded8
Miguel Duarte <gamer-go@hotmail.com>
2018-01-20 03:10:32 +0000

    Finished spotify getter script, now am configuring modules in polybar config file, added fontawesome icons and was nowadding animations to the battery part, slight pause for now

--- .config/polybar/scripts/spotify_polybar.sh
+++ .config/polybar/scripts/spotify_polybar.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env sh
 
-STATUS="$(playerctl status)"
-
-echo "Here goes";
-echo "$STATUS";
-
-if [ "$STATUS" == "Paused" ]
-then
-	echo "Pzd";
-elif [ "$STATUS" == "Playing" ]
-then
-	echo "Pl";
-	echo "$(playerctl metadata artist) - $(playerctl metadata title)"
-else
-	echo "Idk";
+#If spotify is not running, output empty and leave
+if ! pgrep -x spotify > /dev/null; then
+	echo ""; exit
 fi
+
+STATUS=$(playerctl status)
+PAUSED_SEP=""
+PLAYING_SEP=""
+
+if [ $STATUS = "Paused" ]
+then
+	SEPARATOR=$PAUSED_SEP
+elif [ $STATUS = "Playing" ]
+then
+	SEPARATOR=$PLAYING_SEP
+else
+	echo "Unknown player state";
+fi
+
+echo "$SEPARATOR $(playerctl metadata artist) - $(playerctl metadata title)"
