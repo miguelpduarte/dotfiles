decf4ed42025e161c9e870b157b62837ea606c26
miguelpduarte <miguelpduarte98@gmail.com>
2018-11-03 21:00:21 +0000

    Added scripts to enable and disable X.org default screen saving (screen turning off)

---
+++ .config/i3/scripts/disable_screen_timeout.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# According to https://askubuntu.com/questions/763994/screen-times-out-in-i3-wm
+
+# Disabling screen saver
+xset s off
+# Turning off power saving
+xset -dpms
---
+++ .config/i3/scripts/enable_screen_timeout.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# Also according to https://askubuntu.com/questions/763994/screen-times-out-in-i3-wm
+
+# Re-enabling screen saver
+xset s on
+# Turning on power saving
+xset +dpms
