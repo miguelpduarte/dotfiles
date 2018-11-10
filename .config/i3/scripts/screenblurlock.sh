a968bd553b7ade07cfebbcc82d5c367a40fd42c4
miguelpduarte <miguelpduarte98@gmail.com>
2018-11-10 21:42:00 +0000

    Added dunst to the fun

--- .config/i3/scripts/screenblurlock.sh
+++ .config/i3/scripts/screenblurlock.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env sh
 
 #overlay="$HOME/.config/i3/res/overlay.png"
 tmpimg="/tmp/lockpic.png"
@@ -16,8 +16,19 @@ convert "$tmpimg" -scale 10% -scale 1000% "$tmpimg"
 #Apply an icon in the middle of the screen
 #convert "$tmpimg" "$icon" -gravity center -composite -matte "$tmpimg"
 
-#add the overlay located at ~/.cheeselock/arch-logo-2.png to the blurred screenshot and save that as "$tmpimg"
-#composite ~/.cheeselock/arch-logo-2.png "$tmpimg" "$tmpimg"
+# Add the overlay located by $overlay to the blurred screenshot and save that as "$tmpimg"
+#composite "$overlay "$tmpimg" "$tmpimg"
 
-#start i3lock with the overlayed + blurred picture at "$tmpimg"
-i3lock -i "$tmpimg"
+
+# Pausing dunst notifications
+killall -SIGUSR1 dunst
+
+# start i3lock with the overlayed + blurred picture at "$tmpimg"
+# not forking in order to pause dunst notifications before locking and unpausing after locking (if forking, then both would run and everything would be the same)
+# Idea from https://faq.i3wm.org/question/5654/how-can-i-disable-notifications-when-the-screen-locks-and-enable-them-again-when-unlocking/index.html
+
+
+i3lock -i "$tmpimg" --nofork
+
+# Unpausing dunst notifications
+killall -SIGUSR2 dunst
