e52c950afbfeda86496b6033d058d3052b2715d9
miguelpduarte <miguelpduarte98@gmail.com>
2018-12-28 12:08:59 +0000

    Made screen not relock itself endlessly

--- .config/i3/scripts/screenblurlock.sh
+++ .config/i3/scripts/screenblurlock.sh
@@ -19,16 +19,19 @@ convert "$tmpimg" -scale 10% -scale 1000% "$tmpimg"
 # Add the overlay located by $overlay to the blurred screenshot and save that as "$tmpimg"
 #composite "$overlay "$tmpimg" "$tmpimg"
 
-
 # Pausing dunst notifications
 killall -SIGUSR1 dunst
+
+# Suspending xautolock - we don't want to try to lock multiple times...
+pkill xautolock
 
 # start i3lock with the overlayed + blurred picture at "$tmpimg"
 # not forking in order to pause dunst notifications before locking and unpausing after locking (if forking, then both would run and everything would be the same)
 # Idea from https://faq.i3wm.org/question/5654/how-can-i-disable-notifications-when-the-screen-locks-and-enable-them-again-when-unlocking/index.html
-
-
 i3lock -i "$tmpimg" --nofork
 
 # Unpausing dunst notifications
 killall -SIGUSR2 dunst
+
+# Resuming xautolock (does not deal with being in "coffee mode" after unlocking, but oh well, better to "over lock" than to "under lock")
+(~/.config/i3/scripts/screen_timeout_lock.sh &)
