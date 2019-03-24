57311c208d6da4fd39ddafd97388e361a20dba6a
miguelpduarte <miguelpduarte98@gmail.com>
2019-03-24 13:15:38 +0000

    Fixed garbage being printed to terminal by pywal

--- .config/i3/scripts/walscript.sh
+++ .config/i3/scripts/walscript.sh
@@ -7,7 +7,7 @@ walldir="$HOME/Pictures/Wallpapers/alltogethernow"
 #Lists the sources in the wallpaper directory then picks a random one
 #If it can read the wallpaper then it passes the wallpaper to wal so it does its job (just to prevent from passing in a bad path to wal)
 ls $walldir | sort -R | tail -$N | while read wallpaper; do
-	wal -i $walldir/$wallpaper
+	wal -i $walldir/$wallpaper --vte
 	break;
 done
 
