09c3c172457e4abcecedb0bda6e522cf50d868c9
miguelpduarte <miguelpduarte98@gmail.com>
2019-06-18 17:04:15 +0100

    Removed old named files

--- utils/average_last_col.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-less $1 | tail -n +2 | awk '{sum+=$NF} END{print "Average grade: ", sum/NR}'
--- utils/sort_last_col.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-less $1 | tail -n +2 | awk '{print $NF}' | sort
