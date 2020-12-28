8bafa334ba8387e960ea09f1db5151644576e511
miguelpduarte <miguelpduarte98@gmail.com>
2020-12-28 12:48:45 +0000

    Update zim backup script to send logs to logger

--- utils/zim_gdrive_sync.sh
+++ utils/zim_gdrive_sync.sh
@@ -10,3 +10,9 @@ rclone sync "$HOME/Zim Notebooks" gdrive:'Zim Notebooks' \
 exit_val="$?"
 
 notify-send "Ran Zim Notebooks backup with exit code: $exit_val"
+
+if [ "$exit_val" -eq 0 ]; then
+    echo 'ZIM_GDRIVE: Zim backups successful' | /usr/bin/logger -i
+else
+    echo 'ZIM_GDRIVE: Zim backups errored!' | /usr/bin/logger -i
+fi
