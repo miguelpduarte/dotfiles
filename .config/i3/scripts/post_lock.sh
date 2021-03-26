a749ef8db820159b6ae2db335e809272c2a4343b
miguelpduarte <miguelpduarte98@gmail.com>
2021-03-26 17:32:54 +0000

    Update pre and postlock scripts since dunst is being used again

--- .config/i3/scripts/post_lock.sh
+++ .config/i3/scripts/post_lock.sh
@@ -3,8 +3,10 @@
 ## For commands that need to happen after unlocking the screen
 ## Things such as resuming notifications, resuming autolocker, etc
 
-# Resuming dunst notification daemon (not being used atm)
+# Resuming dunst notification daemon
+## Sadly this does not handle the case in which I had the notifications paused before locking and wish that they would remain paused, but I don't see an easy way to handle this atm, so I'll just rely on myself to pause them again
 # killall -SIGUSR2 dunst
+dunstctl set-paused false
 
 # Resuming xautolock (like in pre_lock.sh, if not running exits with error but it's ok)
 xautolock -enable
--- .config/i3/scripts/pre_lock.sh
+++ .config/i3/scripts/pre_lock.sh
@@ -3,8 +3,9 @@
 ## For commands that need to happen before locking the screen
 ## Things such as pausing notifications, pausing autolocker, etc
 
-# Pausing dunst notification daemon (not being used atm)
+# Pausing dunst notification daemon (don't show notifications on the lock screen)
 # killall -SIGUSR1 dunst
+dunstctl set-paused true
 
 # Suspending xautolock - we don't want to try to lock multiple times...
 # If it's not running (coffee mode) this exits with error but it's ok
