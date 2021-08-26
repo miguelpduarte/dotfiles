1dd31b4d31acf5b50991acfe7589fa1162bc9c53
miguelpduarte <miguelpduarte98@gmail.com>
2021-08-26 01:36:21 +0100

    Fix issues in bashrc files found by shellcheck

--- .shell-rcs/installs.sh
+++ .shell-rcs/installs.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# Adding standalone executables to path
-export PATH="/usr/local/sicstus4.4.1/bin/:$PATH"
-export PATH="/home/miguel/Software/postman/Postman/:$PATH"
+## Adding standalone executables and libraries to path
+# export PATH="/usr/local/sicstus4.4.1/bin/:$PATH"
+# export PATH="/home/miguel/Software/postman/Postman/:$PATH"
+# Performance API (PAPI)
+# export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
 
-# https://asdf-vm.com
+## https://asdf-vm.com
 . $HOME/.asdf/asdf.sh
 . $HOME/.asdf/completions/asdf.bash
 
@@ -12,19 +14,17 @@ export PATH="/home/miguel/Software/postman/Postman/:$PATH"
 export PATH="/home/miguel/utils/:$PATH"
 
 # https://github.com/nvbn/thefuck
-eval $(thefuck --alias)
+eval "$(thefuck --alias)"
 
 # fasd: https://github.com/clvv/fasd
 # eval "$(fasd --init auto)" # Simple version. The below version caches fasd init code for minimal overhead on starting up the shell
 fasd_cache="$HOME/.fasd-init-bash"
-if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+if [ "$(command -v fasd)" -nt "$fasd_cache" ] || [ ! -s "$fasd_cache" ]; then
   fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
 fi
 source "$fasd_cache"
 unset fasd_cache
 
-# Performance API (PAPI)
-export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
 
 # Getting python virtualenv manager aliases and functions
 . "$HOME/.shell-rcs/python-venv-manager.sh"
