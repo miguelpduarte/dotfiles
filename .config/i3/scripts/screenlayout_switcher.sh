03a59374bee0b95161d8f3f8166564e8da294863
miguelpduarte <miguelpduarte98@gmail.com>
2023-01-08 00:19:26 +0100

    Always relaunch polybar when starting i3 (trying to minimize issues when restarting i3). Make sure the third monitor has its own workspace if connected so that keybinds are more helpful. When switching screenlayour, relaunch polybar so it picks up the monitor changes

--- .config/i3/scripts/screenlayout_switcher.sh
+++ .config/i3/scripts/screenlayout_switcher.sh
@@ -2,4 +2,5 @@
 
 selected_layout="$(find ~/.screenlayout/ -type f | rofi -dmenu -i -p "Monitor Setup")"
 
-[ -n "$selected_layout" ] && $selected_layout
+# Relaunching polybar if the switch was successful since that's the fix I usually do by hand anyway
+[ -n "$selected_layout" ] && $selected_layout && ~/.config/polybar/launch.sh
