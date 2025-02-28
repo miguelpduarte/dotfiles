68f7de84bc044a1c2520748b6a288ff7293037d2
Miguel Duarte <miguelpduarte98@gmail.com>
2025-02-28 17:40:18 +0100

    nix-darwin: Update stateVersion due to build user group ID change
    It might also just have already been correct but now it's checked and
    the check was expecting the outdated version.
    
    See: https://github.com/LnL7/nix-darwin/blob/5d6e0851b60508cffd66b4a6982440a40720338d/CHANGELOG#L21

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -117,7 +117,7 @@
 
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
-      system.stateVersion = 4;
+      system.stateVersion = 5;
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
