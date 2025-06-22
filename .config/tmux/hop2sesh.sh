dafbd70632d1414830b388cd8ff0a2281b6890d1
Miguel Duarte <miguelpduarte98@gmail.com>
2025-06-22 21:04:53 +0200

    tmux: Fix empty input crashing command

--- .config/tmux/hop2sesh.sh
+++ .config/tmux/hop2sesh.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # https://sipb.mit.edu/doc/safe-shell/
-set -euf -o pipefail
+set -uf -o pipefail
+# no -e because fzf exit code doesn't matter, we just want to check the output
 
 sesh="$(tmux list-sessions -F\#S | fzf --tmux)";
 
