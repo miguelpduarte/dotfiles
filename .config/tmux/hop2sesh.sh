2318cbd81d83f6563fccc79c764adcb10595b046
Miguel Duarte <miguelpduarte98@gmail.com>
2025-12-14 13:54:57 +0100

    tmux: Also cleaner cool sesh picker + docs

--- .config/tmux/hop2sesh.sh
+++ .config/tmux/hop2sesh.sh
@@ -4,7 +4,7 @@
 set -uf -o pipefail
 # no -e because fzf exit code doesn't matter, we just want to check the output
 
-sesh="$(tmux list-sessions -F\#S | fzf --tmux)";
+sesh="$(tmux list-sessions -F\#S | fzf)";
 
 if [ -n "$sesh" ]; then
 	tmux switch-client -t "$sesh"
